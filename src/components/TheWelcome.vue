38e3c8014aa0a0d687d419e7b86942e19833413d
Alex <alex-fil@mail.ru>
2024-10-03 00:40:45 +0300

    стили

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -7,9 +7,9 @@
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
 import * as THREE from "three";
-import { CSG } from "three-csg-ts";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import woodTexture from "@/assets/images/wood-door.jpg";
+import wallTextureUrl from "@/assets/images/bricks-wall.jpg";
 
 const container = ref<HTMLDivElement | null>(null);
 const canvasElement = ref<HTMLCanvasElement | null>(null);
@@ -19,26 +19,42 @@ onMounted(() => {
   if (container.value) {
     // Создание сцены
     const scene = new THREE.Scene();
-    // CSG.init(THREE);
-    // Создание камеры
 
-    // Создание сферы
-    // const geometry = new THREE.SphereGeometry(1, 32, 32);
-    // const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
-    // const sphere = new THREE.Mesh(geometry, material);
-    // scene.add(sphere);
+    const wallTexture = new THREE.TextureLoader().load(wallTextureUrl);
+    wallTexture.repeat.set(1.2, 1.2);
+    wallTexture.offset.set(0, 0);
+    const materialWall = new THREE.MeshStandardMaterial({
+      color: 0xc68c53,
+      map: wallTexture,
+      metalness: 0.9,
+      roughness: 0.1,
+    });
 
-    // // Фокусировка камеры на сфере
-    // camera.lookAt(sphere.position);
+    const materialLight = new THREE.MeshStandardMaterial({
+      color: 0xf5f5dc,
+      map: new THREE.TextureLoader().load(woodTexture),
+      metalness: 0.2,
+      roughness: 0.8,
+    });
+    const material = new THREE.MeshStandardMaterial({
+      color: 0xd2b48c,
+      map: new THREE.TextureLoader().load(woodTexture),
+      metalness: 0.7,
+      roughness: 0.5,
+    });
+    const materialKnob = new THREE.MeshStandardMaterial({
+      color: 0x808080,
+      map: new THREE.TextureLoader().load(woodTexture),
+      metalness: 0.1,
+      roughness: 0.9,
+    });
 
-    // Создание источника света
-
-    const ambientLight = new THREE.AmbientLight(0x404040); // Ambient light
+    const ambientLight = new THREE.AmbientLight(0x404040);
     scene.add(ambientLight);
 
     const spotLight = new THREE.SpotLight(0xffffff);
-    spotLight.position.set(0, 2, 0); // Поместите источник света внутрь комнаты
-    spotLight.intensity = 1.5; // Увеличиваем яркость источника света
+    spotLight.position.set(3, 6.5, 3);
+    spotLight.intensity = 3;
     spotLight.castShadow = true;
     spotLight.shadow.mapSize.width = 1024;
     spotLight.shadow.mapSize.height = 1024;
@@ -50,48 +66,47 @@ onMounted(() => {
     const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x404040, 0.6);
     scene.add(hemisphereLight);
 
-    const wallMaterial = new THREE.MeshPhongMaterial({ color: 0x808080 }); // Grey color
     // создание стены wallA
     const wallAGeometry = new THREE.BoxGeometry(6, 3.2, 0.3);
-    const wallA = new THREE.Mesh(wallAGeometry, wallMaterial);
+    const wallA = new THREE.Mesh(wallAGeometry, materialWall);
     wallA.position.set(2, 4.8, 2);
     scene.add(wallA);
 
     // создание стены wallB
     const wallBGeometry = new THREE.BoxGeometry(3, 3.2, 0.3);
-    const wallB = new THREE.Mesh(wallBGeometry, wallMaterial);
+    const wallB = new THREE.Mesh(wallBGeometry, materialWall);
     wallB.position.set(-1, 4.8, 3.35);
-    wallB.rotation.y = Math.PI / 2; // Rotate 90 degrees around the X-axis
+    wallB.rotation.y = Math.PI / 2;
     scene.add(wallB);
 
     // создание стены wallB
     const wallCGeometry = new THREE.BoxGeometry(3, 3.2, 0.3);
-    const wallC = new THREE.Mesh(wallCGeometry, wallMaterial);
-    wallC.position.set(5, 4.8, 3.35); // Установка позиции стены wallC симметрично относительно середины стены wallA
-    wallC.rotation.y = Math.PI / 2; // Поворот на 90 градусов вокруг оси Y
+    const wallC = new THREE.Mesh(wallCGeometry, materialWall);
+    wallC.position.set(5, 4.8, 3.35);
+    wallC.rotation.y = Math.PI / 2;
     scene.add(wallC);
 
     // Создание box по периметру стен
-    const floorBottomGeometry = new THREE.BoxGeometry(6.6, 0.3, 2.65); // Размеры box
-    const floorBottom = new THREE.Mesh(floorBottomGeometry, wallMaterial);
-    floorBottom.position.set(2, 3.2, 3.675); // Позиция box
+    const floorBottomGeometry = new THREE.BoxGeometry(6.6, 0.3, 2.65);
+    const floorBottom = new THREE.Mesh(floorBottomGeometry, materialWall);
+    floorBottom.position.set(2, 3.2, 3.675);
     scene.add(floorBottom);
 
-    // Floor Top (Ceramic Tiles)
-    const tileSize = 0.4; // 400mm x 400mm
-    const tileSpacing = 0.02; // 2cm spacing between tiles
+    // Floor Top
+    const tileSize = 0.4;
+    const tileSpacing = 0.02;
     const tilesPerRow = Math.floor(6.6 / (tileSize + tileSpacing));
     const tilesPerColumn = Math.floor(3.65 / (tileSize + tileSpacing));
 
-    const tileGeometry = new THREE.BoxGeometry(tileSize, 0.05, tileSize); // Увеличили толщину до 5 см
-    const tileMaterial = new THREE.MeshPhongMaterial({ color: 0xffffff }); // White ceramic tiles
+    const tileGeometry = new THREE.BoxGeometry(tileSize, 0.05, tileSize);
+    const tileMaterial = new THREE.MeshPhongMaterial({ color: 0xffffff });
 
     for (let x = 0; x < tilesPerRow; x++) {
       for (let z = 0; z < tilesPerColumn; z++) {
         const tile = new THREE.Mesh(tileGeometry, tileMaterial);
         tile.position.set(
           2 - 6.6 / 2 + (tileSize + tileSpacing) * x,
-          3.35, // Slightly above the floor bottom
+          3.35,
           3.675 - 3.65 / 2 + (tileSize + tileSpacing) * z
         );
         scene.add(tile);
@@ -110,30 +125,7 @@ onMounted(() => {
       Math.PI * 0.444
     );
     const textureLoader = new THREE.TextureLoader();
-    // const woodTexture = textureLoader.load("../assets/images/wood-door");
-    // const cubeTexture = textureLoader.load([
-    //   "px.png",
-    //   "nx.png",
-    //   "py.png",
-    //   "ny.png",
-    //   "pz.png",
-    //   "nz.png",
-    // ]);
-    // Создаем материал
-    const materialLight = new THREE.MeshStandardMaterial({
-      color: 0xf5f5dc, // Beige color
-      map: new THREE.TextureLoader().load(woodTexture),
-      metalness: 0.2, // Настройка металличности
-      roughness: 0.8, // Настройка шероховатости
-      // envMap: cubeTexture, // Использование фона сцены как отражение
-    });
-    const material = new THREE.MeshStandardMaterial({
-      color: 0xd2b48c, // Коричневый цвет
-      map: new THREE.TextureLoader().load(woodTexture),
-      metalness: 0.7, // Настройка металличности
-      roughness: 0.5, // Настройка шероховатости
-      // envMap: cubeTexture, // Использование фона сцены как отражение
-    });
+
     // Создаем координаты для прямоугольника
     const x1 = -0.04,
       z1 = 0.02;
@@ -166,16 +158,15 @@ onMounted(() => {
       new THREE.Vector2(x3, z3),
       new THREE.Vector2(x4, z4),
     ]);
-    // const shapeGeometry = new THREE.ShapeGeometry(shape);
 
     const extrudeSettings = {
-      depth: 2.5, // Extrude depth
-      bevelEnabled: false, // Disable bevel
+      depth: 2.5,
+      bevelEnabled: false,
     };
     const shapeGeometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
     // const lines = new THREE.LineSegments(geometry, material);
     const mesh = new THREE.Mesh(shapeGeometry, materialLight);
-    mesh.rotation.x = Math.PI / 2; // Rotate 90 degrees around the X-axis
+    mesh.rotation.x = Math.PI / 2;
     mesh.position.set(2, 5.6, 2.2);
 
     scene.add(mesh);
@@ -201,33 +192,17 @@ onMounted(() => {
     niche.position.set(2, 2, 2);
     scene.add(niche);
 
-    // Make 2 meshes..
-    const box = new THREE.Mesh(
-      new THREE.BoxGeometry(2, 2, 2),
-      new THREE.MeshNormalMaterial()
-    );
-    box.position.set(0, 1, 0); // Set the position of the box
+    // door knob
+    const doorKnobGeometry = new THREE.SphereGeometry(0.06, 12, 12); // Радиус 0.1, 16 сегментов по ширине и высоте
+    const doorKnobMaterial = new THREE.MeshPhongMaterial({ color: 0x8b4513 }); // Коричневый цвет
+    // Создание дверной ручки
+    const doorKnob = new THREE.Mesh(doorKnobGeometry, doorKnobMaterial);
+    doorKnob.position.set(-0.25, 2.35, 0.2); // Позиция ручки на высоте 0.9 от плитки
+    niche.add(doorKnob);
 
-    const sphere = new THREE.Mesh(new THREE.SphereGeometry(1.2, 8, 8));
-    sphere.position.set(0, 1, 0); // Position the sphere at the same position as the box
-
-    // Make sure the .matrix of each mesh is current
-    box.updateMatrix();
-    sphere.updateMatrix();
-
-    // Perform CSG operations
-    const subRes = CSG.subtract(box, sphere);
-
-    // Add the subtracted result to the scene
-    scene.add(subRes);
-
-    // Create a door
-    const doorGeometry = new THREE.BoxGeometry(0.9, 2.1, 0.1);
-    const doorMaterial = new THREE.MeshPhongMaterial({ color: 0x8b4513 });
-    const door = new THREE.Mesh(doorGeometry, doorMaterial);
-    door.position.set(2, 1.05, -3); // Поместите дверь параллельно окну
-
-    scene.add(door);
+    const doorKnob2 = new THREE.Mesh(doorKnobGeometry, doorKnobMaterial);
+    doorKnob2.position.set(0.25, 2.35, 0.2); // Позиция ручки на высоте 0.9 от плитки, симметрично оси ниши
+    niche.add(doorKnob2); // Добавляем ручку к нише
 
     // Создание рендерера
     const renderer = new THREE.WebGLRenderer();
@@ -239,7 +214,7 @@ onMounted(() => {
       0.1,
       1000
     );
-    camera.position.set(0, 7, 7); // Set the camera position higher
+    camera.position.set(0, 6, 6); // Set the camera position higher
     camera.rotation.x = Math.PI / 2; // Rotate the camera 90 degrees around the X-axis
 
     // camera.lookAt(door.position); // Set the camera target
@@ -257,7 +232,7 @@ onMounted(() => {
     function animate() {
       requestAnimationFrame(animate);
       controls?.update();
-      camera.lookAt(3, 3, 3);
+      camera.lookAt(-0.5, 3.9, 0.35);
       renderer.render(scene, camera);
     }
     animate();
